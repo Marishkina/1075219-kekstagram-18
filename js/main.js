8c70243da8849769366c8749d79ba0826c4c6c5f
Marina <marishkina@gmail.com>
2019-10-12 13:59:09 +0800

    Добавляет массив объектов - комментарии, добавляет iife

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Добавляет массив объектов - комментарии, добавляет iife
 (function () {
 
   var PHOTOS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
@@ -87,6 +90,7 @@
 
   init(generateListOfPhotos(PHOTO_ITEMS_COUNT));
 })();
+<<<<<<< HEAD
 =======
 var PHOTOS = ['photos/1.jpg', 'photos/2.jpg', 'photos/3.jpg', 'photos/4.jpg', 'photos/5.jpg', 'photos/6.jpg', 'photos/7.jpg', 'photos/8.jpg', 'photos/9.jpg', 'photos/10.jpg', 'photos/11.jpg', 'photos/12.jpg', 'photos/13.jpg', 'photos/14.jpg', 'photos/15.jpg', 'photos/16.jpg', 'photos/17.jpg', 'photos/18.jpg', 'photos/19.jpg', 'photos/20.jpg', 'photos/21.jpg', 'photos/22.jpg', 'photos/23.jpg', 'photos/24.jpg', 'photos/25.jpg'];
 var DESCRIPTIONS = ['фото огонь', 'так себе настроение', 'позитивчик', 'отдыхаю'];
@@ -167,3 +171,5 @@ init();
 
 init(generateListOfPhotos(PHOTO_ITEMS_COUNT));
 >>>>>>> Делает функцию init - функциональным выражением, правит название переменных и меняет функцию init
+=======
+>>>>>>> Добавляет массив объектов - комментарии, добавляет iife
