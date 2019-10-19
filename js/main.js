e97088083509db2186e1795785fab249fca9e726
Marina <marishkina@gmail.com>
2019-10-19 11:37:14 +0800

    Правки задание 3-3

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@
   var NAMES = ['Артем', 'Вася', 'Матрена', 'Катюша', 'Слава', 'Поля'];
   var AVATARS = ['1', '2', '3', '4', '5', '6'];
   var PHOTO_ITEMS_COUNT = 25;
+  var ENTER_KEYCODE = 13;
+  var ESC_KEYCODE = 27;
 
   var picturesList = document.querySelector('.pictures');
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -41,10 +43,8 @@
       var shuffledListComments = shuffleList(COMMENTS);
       var listOfMessage = shuffledListComments.slice(0, getRandomNumber(MIN_COMMENTS, MAX_COMMENTS));
 
-      var name = getRandomItem(AVATARS);
-
       listOfComments = {
-        avatar: 'img/avatar-' + name + '.svg',
+        avatar: 'img/avatar-' + getRandomItem(AVATARS) + '.svg',
         message: listOfMessage,
         name: getRandomItem(NAMES)
       };
@@ -77,9 +77,8 @@
     return pictureElement;
   };
 
-  // задание - личный проект: больше деталей в module3-task3
   var bigPicture = document.querySelector('.big-picture');
-  var socialComment = bigPicture.querySelector('.social__comment');
+  var main = document.querySelector('main');
   var commentsCount = bigPicture.querySelector('.social__comment-count');
   var commentsLoader = bigPicture.querySelector('.comments-loader');
   var bigPictureCancel = bigPicture.querySelector('#picture-cancel');
@@ -98,7 +97,7 @@
     }
   };
 
-  var closeBigPicturEnterDown = function () {
+  var closeBigPictureEnterDown = function () {
     if (bigPictureCancel.target === ENTER_KEYCODE) {
       bigPicture.classList.add('hidden');
     }
@@ -109,7 +108,7 @@
   commentsLoader.classList.add('visually-hidden');
   bigPictureCancel.addEventListener('click', closeBigPicture);
   document.addEventListener('keydown', onBigPictureEscDown);
-  bigPictureCancel.addEventListener('keydown', closeBigPicturEnterDown);
+  bigPictureCancel.addEventListener('keydown', closeBigPictureEnterDown);
 
   var generateBigPictureElements = function (generateListItems) {
 
@@ -123,7 +122,6 @@
 
     return bigPicture;
   };
-  // задание 3-3 финиш
 
   var init = function (generateListItems) {
     var fragment = document.createDocumentFragment();
@@ -131,131 +129,10 @@
     for (var i = 0; i < PHOTO_ITEMS_COUNT; i++) {
       fragment.appendChild(generatePhoto(generateListItems[i]));
     }
+
     picturesList.appendChild(fragment);
-    socialComment.appendChild(generateBigPictureElements(generateListItems));
+    main.appendChild(generateBigPictureElements(generateListItems));
   };
 
   init(generateListOfPhotos(PHOTO_ITEMS_COUNT));
-
-  var uploadForm = document.querySelector('.img-upload__form');
-  var uploadFieldset = uploadForm.querySelector('.img-upload__start');
-  var uploadButton = uploadFieldset.querySelector('#upload-file');
-  var ENTER_KEYCODE = 13;
-  var ESC_KEYCODE = 27;
-  var TAB_KEYCODE = 9;
-  var fileName = uploadFieldset.querySelector('input[name = filename]');
-  var uploadOverlayForm = uploadForm.querySelector('.img-upload__overlay');
-  var closeButtonUploadOverlayForm = uploadOverlayForm.querySelector('#upload-cancel');
-  var effectLevel = uploadForm.querySelector('.img-upload__effect-level');
-  // var effectLevelDepth = effectLevel.querySelector('.effect-level__depth');
-  var pinEffectLevel = effectLevel.querySelector('.effect-level__pin');
-  // var effectLevelValue = effectLevel.querySelector('.effect-level__value');
-  var effects = uploadForm.querySelector('.img-upload__effects');
-  var effectsItems = effects.querySelector('.effects__item');
-  var hashtagFieldset = uploadForm.querySelector('.img-upload__text');
-  var hashtagInput = hashtagFieldset.querySelector('input[name=hashtags]');
-  var MAX_HASHTAGS = 5;
-  var MAX_HASHTAG_LENGTH = 20;
-
-  var openUploadFieldset = function () {
-    uploadButton.classList.remove('visually-hidden');
-  };
-
-  var closeUploadFieldset = function () {
-    uploadButton.classList.add('visually-hidden');
-  };
-
-  var openUploadOverlayForm = function () {
-    uploadOverlayForm.classList.remove('hidden');
-  };
-
-  var onUploadFieldsetEnterDown = function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      openUploadFieldset();
-    }
-  };
-
-  var onUploadFieldsetEscDown = function (evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
-      closeUploadFieldset();
-    }
-  };
-
-  var closeUploadFieldsetHandler = function (evt) {
-    if (evt.target !== fileName) {
-      closeUploadFieldset();
-    }
-  };
-
-  var onUploadFieldsetTabDown = function (evt) {
-    if (evt.keyCode === TAB_KEYCODE) {
-      closeUploadFieldset();
-    }
-  };
-
-  var photoSelection = function () {
-    uploadForm.reset();
-    openUploadOverlayForm();
-  };
-
-  var closeUploadOverlayForm = function () {
-    uploadOverlayForm.classList.add('hidden');
-  };
-
-  var onUploadOverlayFormEscDown = function (evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
-      if (evt.target === hashtagInput) {
-        evt.stopPropogation();
-      } else {
-        closeUploadOverlayForm();
-      }
-    }
-  };
-
-  // определение уровня насыщенности и нахождение положения пина относительно блока effectLevel
-  var pinEffectLevelPosition = function (evt) {
-    return evt.pageX - pinEffectLevel.offsetLeft;
-  };
-
-  var depthChange = function (maxFilter) {
-    return (pinEffectLevelPosition() * maxFilter) / 100;
-  };
-
-  // сброс уровня эффекта до начального состояния
-  var effectLevelReset = function () {
-    effectLevel.reset();
-  };
-
-  // валидация хэш-тегов
-
-  var validateHashtags = function () {
-    var a = hashtagInput.toLowerCase();
-    var hashtagList = a.split(' ');
-
-    for (var i = 0; i < hashtagList.length; i++) {
-      if (hashtagList[i].indexOf('#') !== 0) {
-        hashtagInput.setCustomValidity('хэш-тег начинается с символа #');
-      } else if (hashtagList[i] === 1 && hashtagList[i].indexOf('#') === 0) {
-        hashtagInput.setCustomValidity('хеш-тег не может состоять только из одной решётки');
-      } else if (hashtagList.include(hashtagList[i])) {
-        hashtagInput.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');
-      } else if (hashtagList.length > MAX_HASHTAGS) {
-        hashtagInput.setCustomValidity('максимум пять хэш-тегов');
-      } else if (hashtagList[i].length > MAX_HASHTAG_LENGTH) {
-        hashtagInput.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');
-      }
-    }
-  };
-
-  uploadFieldset.addEventListener('click', openUploadFieldset);
-  uploadFieldset.addEventListener('keydown', onUploadFieldsetEnterDown);
-  document.addEventListener('keydown', onUploadFieldsetEscDown);
-  document.addEventListener('click', closeUploadFieldsetHandler);
-  document.addEventListener('keydown', onUploadFieldsetTabDown);
-  fileName.addEventListener('change', photoSelection);
-  closeButtonUploadOverlayForm.addEventListener('click', closeUploadOverlayForm);
-  document.addEventListener('keydown', onUploadOverlayFormEscDown);
-  effectLevel.addEventListener('mouseup', depthChange);
-  effectsItems.addEventListener('change', effectLevelReset); // сброс уровня эффекта до начального состояния при переключении фильтра
-  hashtagInput.addEventListener('input', validateHashtags);
 })();
