83a500311ba843980abaf0fd0ad4950c60219990
Marishkina <42662547+Marishkina@users.noreply.github.com>
2019-10-21 21:12:27 +0800

    Личный проект: больше деталей (#5)
    * Правит функцию renderComments
    
    * Правит название функций, выводит комментарии
    
    * Убирает лишнее из функции commentsArray
    
    * Правит название функций, правит счетчик комментов и их вывод
    
    * Правит функцию init, названия переменных
    
    * Правит название переменной COUNT
    
    * Правит название переменных
    
    * Делает функцию init - функциональным выражением, правит название переменных и меняет функцию init
    
    * Добавляет массив объектов - комментарии, добавляет iife
    
    * Задание Личный проект: побробности
    
    * Показывает форму редактирования, эффект насыщенности фильтра, валидация хэш-тегов
    
    * Правка по Тревису
    
    * Добавляет большое фото
    
    * Задание 3-3
    
    * Правки задание 3-3
    
    * Выносит вывод коммента в другую функцию
    
    * Правит функцию генерации массива с комментами
    
    * Задание 3-3: выводит комментарии к большому фото
    
    * Задание 3-3: правит функцию отрисовки li
    
    * Убирает лишнее из кода, переименовывает функцию init
    
    * Правит функцию comment

--- js/main.js
+++ js/main.js
@@ -4,13 +4,20 @@
 
   var PHOTOS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
   var DESCRIPTIONS = ['фото огонь', 'так себе настроение', 'позитивчик', 'отдыхаю'];
-  var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у нее получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  var COMMENTS = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у нее получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
   var MIN_COMMENTS = 1;
-  var MAX_COMMENTS = 2;
+  var MAX_COMMENTS = 120;
   var MIN_LIKES = 15;
   var MAX_LIKES = 200;
   var NAMES = ['Артем', 'Вася', 'Матрена', 'Катюша', 'Слава', 'Поля'];
-  var AVATARS = ['avatar-1.svg', 'avatar-2.svg', 'avatar-3.svg', 'avatar-4.svg', 'avatar-5.svg', 'avatar-6.svg'];
+  var AVATARS = ['1', '2', '3', '4', '5', '6'];
   var PHOTO_ITEMS_COUNT = 25;
 
   var picturesList = document.querySelector('.pictures');
@@ -35,17 +42,16 @@
   };
 
   var comments = function (count) {
+    var shuffledListComments = shuffleList(COMMENTS);
     var listOfComments = [];
-
     for (var i = 0; i < count; i++) {
-      var shuffledListComments = shuffleList(COMMENTS);
-      var listOfMessage = shuffledListComments.slice(0, getRandomNumber(MIN_COMMENTS, MAX_COMMENTS));
 
-      listOfComments = {
-        avatar: getRandomItem(AVATARS),
-        message: listOfMessage,
+      var comment = {
+        avatar: 'img/avatar-' + getRandomItem(AVATARS) + '.svg',
+        message: getRandomItem(shuffledListComments),
         name: getRandomItem(NAMES)
       };
+      listOfComments.push(comment);
     }
     return listOfComments;
   };
@@ -59,7 +65,7 @@
         url: 'photos/' + shuffledListPhotos[i] + '.jpg',
         description: getRandomItem(DESCRIPTIONS),
         likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-        comments: comments(PHOTO_ITEMS_COUNT)
+        comments: comments(getRandomNumber(MIN_COMMENTS, MAX_COMMENTS))
       };
     }
     return itemsOfListOfPhotos;
@@ -70,19 +76,67 @@
 
     pictureElement.querySelector('.picture__img').src = itemsListPhoto.url;
     pictureElement.querySelector('.picture__likes').textContent = itemsListPhoto.likes;
-    pictureElement.querySelector('.picture__comments').textContent = String(itemsListPhoto.comments.message.length);
+    pictureElement.querySelector('.picture__comments').textContent = String(itemsListPhoto.comments.length);
 
     return pictureElement;
   };
 
-  var init = function (generateListItems) {
+  var bigPicture = document.querySelector('.big-picture');
+  var socialComments = bigPicture.querySelector('.social__comments');
+  var socialComment = socialComments.querySelector('.social__comment');
+  var commentsCount = bigPicture.querySelector('.social__comment-count');
+  var commentsLoader = bigPicture.querySelector('.comments-loader');
+
+  bigPicture.classList.remove('hidden');
+
+  commentsCount.classList.add('visually-hidden');
+  commentsLoader.classList.add('visually-hidden');
+
+  // функция собирает li
+  var renderBigPictureComment = function (comment) {
+    var socialCommentElement = socialComment.cloneNode(true);
+
+    socialCommentElement.querySelector('.social__picture').src = comment.avatar;
+    socialCommentElement.querySelector('.social__picture').alt = comment.name;
+    socialCommentElement.querySelector('.social__text').textContent = comment.message;
+
+    return socialCommentElement;
+  };
+
+  // функция собирает li в ul
+  var renderListOfComments = function (listOfComments) {
+    socialComments.innerHTML = '';
+
+    var commentsFragment = document.createDocumentFragment();
+
+    for (var i = 0; i < listOfComments.length; i++) {
+      commentsFragment.appendChild(renderBigPictureComment(listOfComments[i]));
+    }
+
+    return commentsFragment;
+  };
+
+  var generateBigPictureElements = function (generateListItems) {
+    bigPicture.querySelector('.big-picture__img').src = generateListItems[0].url;
+    bigPicture.querySelector('.likes-count').textContent = generateListItems[0].likes;
+    bigPicture.querySelector('.comments-count').textContent = String(generateListItems[0].comments.length);
+    bigPicture.querySelector('.social__caption').textContent = generateListItems[0].description;
+    bigPicture.querySelector('.social__comments').appendChild(renderListOfComments(generateListItems[0].comments));
+
+    return bigPicture;
+  };
+
+  var generatePhotoPage = function (generateListItems) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < PHOTO_ITEMS_COUNT; i++) {
       fragment.appendChild(generatePhoto(generateListItems[i]));
     }
+
     picturesList.appendChild(fragment);
+
+    generateBigPictureElements(generateListItems);
   };
 
-  init(generateListOfPhotos(PHOTO_ITEMS_COUNT));
+  generatePhotoPage(generateListOfPhotos(PHOTO_ITEMS_COUNT));
 })();
