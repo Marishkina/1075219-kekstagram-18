a3a3125424486843de67c8dfa0f719de9a1a7a54
Marina <marishkina@gmail.com>
2019-10-31 02:18:34 +0800

    Добавляет масштабирование фотографии

--- js/main.js
+++ js/main.js
@@ -24,7 +24,10 @@
   var MIN_SCALE = 25;
   var MAX_SCALE = 100;
   var SCALE_STEP = 25;
+<<<<<<< HEAD
   var MAX_COMMENT_LENGTH = 140;
+=======
+>>>>>>> Добавляет масштабирование фотографии
 
   var picturesList = document.querySelector('.pictures');
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
