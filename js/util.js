207739586ae63112a72b204f2c6eb623cd134c09
Marina <marishkina@gmail.com>
2019-11-04 23:14:12 +0800

    Правит util.js

--- js/util.js
+++ js/util.js
@@ -1,17 +1,11 @@
 'use strict';
 
 (function () {
-  var ENTER_CODE = 'Enter';
   var ESC_CODE = 'Escape';
 
   window.util = {
     isEscEvent: function (evt, action) {
       if (evt.code === ESC_CODE) {
-        action();
-      }
-    },
-    isEnterEvent: function (evt, action) {
-      if (evt.code === ENTER_CODE) {
         action();
       }
     }
