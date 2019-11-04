ba1a0afe054120ca6b13195bafe3b896e492be97
Marina <marishkina@gmail.com>
2019-11-04 13:42:18 +0800

    Задание 5-2: модуляция

---
+++ js/generate-data.js
@@ -0,0 +1,70 @@
+'use strict';
+
+(function () {
+
+  var PHOTOS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
+  var DESCRIPTIONS = ['фото огонь', 'так себе настроение', 'позитивчик', 'отдыхаю'];
+  var COMMENTS = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у нее получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  var MIN_COMMENTS = 1;
+  var MAX_COMMENTS = 120;
+  var MIN_LIKES = 15;
+  var MAX_LIKES = 200;
+  var NAMES = ['Артем', 'Вася', 'Матрена', 'Катюша', 'Слава', 'Поля'];
+  var AVATARS = ['1', '2', '3', '4', '5', '6'];
+
+  var getRandomItem = function (array) {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  var getRandomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  var shuffleList = function (a) {
+    for (var i = a.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
+  };
+
+  var getComments = function (count) {
+    var shuffledListComments = shuffleList(COMMENTS);
+    var listOfComments = [];
+    for (var i = 0; i < count; i++) {
+
+      var comment = {
+        avatar: 'img/avatar-' + getRandomItem(AVATARS) + '.svg',
+        message: getRandomItem(shuffledListComments),
+        name: getRandomItem(NAMES)
+      };
+      listOfComments.push(comment);
+    }
+    return listOfComments;
+  };
+
+  // генерация массива объектов для миниатюры
+  window.generateData = function (count) {
+    var shuffledListPhotos = shuffleList(PHOTOS);
+    var itemsOfListOfPhotos = [];
+
+    for (var i = 0; i < count; i++) {
+      itemsOfListOfPhotos[i] = {
+        url: 'photos/' + shuffledListPhotos[i] + '.jpg',
+        description: getRandomItem(DESCRIPTIONS),
+        likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+        comments: getComments(getRandomNumber(MIN_COMMENTS, MAX_COMMENTS))
+      };
+    }
+    return itemsOfListOfPhotos;
+  };
+})();
