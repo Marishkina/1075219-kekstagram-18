91d5d903cb83bdb1b17ee7b4109a206825242a3c
Marishkina <42662547+Marishkina@users.noreply.github.com>
2019-11-05 17:13:43 +0800

    Личный проект: подробности (#6)
    * Задание Личный проект: побробности
    
    * Показывает форму редактирования, эффект насыщенности фильтра, валидация хэш-тегов
    
    * Правка по Тревису
    
    * Правки задание 4-2: подробности
    
    * Задание 4-2: добавляет большое фото и комментарии
    
    * 4-2
    
    * 4-2-1
    
    * Задание 4-2: подробности, на проверку
    
    * Правит название функций, правит использование keyboardEvent.Code, доюавляет функцию switch
    
    * Правит название переменной
    
    * Правит название функций-обработчиков
    
    * Правит название переменной в функции валидации хеш-тегов, добавляет функцию-обработчик для текстового поля Hashtag
    
    * Правит название переменныч и функцию валидации хеш-тегов
    
    * Правит название переменныч и функцию валидации хеш-тегов
    
    * Удаляет лишнее, разделяет функции
    
    * Добавила обработку по клику открытие выбора фото - удалила нечаянно до этого
    
    * Удаляет обработчики
    
    * Редактирует функцию обработчик - для повторного выбора фото и открытия формы редактирования фото, правит функцию-обработчик - закрытие формы кликом
    
    * Правит валидацию хеш-тегов
    
    * Правит валидацию хеш-тегов, добавляет проверку имвола решетки на 1-м хеш-теге
    
    * Правит валидацаю хеш-тегов, добавляет определение позиции Пина
    
    * Добавляет класс к картинке через if..else
    
    * Добавляет класс к картинке через switch, правит функцию валидации хеш-тегов
    
    * Правит функцию switch для добавления класса к картинке - убирает/меняет 1 класс
    
    * Правит функцию валидации хеш-тегов - проверка на наличие #в первом хеш-теге и убирает лишние слушатели
    
    * Правит функцию switch
    
    * Добавляет масштабирование фотографии
    
    * Правит удаление классов у поля .img-upload__preview
    
    * Переносит установку значения для .image-upload--preview в openUploadOverlayForm и переносит преобразование value в функции кликов
    
    * добавляет пустую строку
    
    * дополняет проверку первого хеш-тега на наличие #

--- js/main.js
+++ js/main.js
@@ -19,6 +19,11 @@
   var NAMES = ['Артем', 'Вася', 'Матрена', 'Катюша', 'Слава', 'Поля'];
   var AVATARS = ['1', '2', '3', '4', '5', '6'];
   var PHOTO_ITEMS_COUNT = 25;
+  var MAX_HASHTAGS_COUNT = 5;
+  var MAX_HASHTAG_LENGTH = 20;
+  var MIN_SCALE = 25;
+  var MAX_SCALE = 100;
+  var SCALE_STEP = 25;
 
   var picturesList = document.querySelector('.pictures');
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -41,7 +46,7 @@
     return a;
   };
 
-  var comments = function (count) {
+  var getComments = function (count) {
     var shuffledListComments = shuffleList(COMMENTS);
     var listOfComments = [];
     for (var i = 0; i < count; i++) {
@@ -65,7 +70,7 @@
         url: 'photos/' + shuffledListPhotos[i] + '.jpg',
         description: getRandomItem(DESCRIPTIONS),
         likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-        comments: comments(getRandomNumber(MIN_COMMENTS, MAX_COMMENTS))
+        comments: getComments(getRandomNumber(MIN_COMMENTS, MAX_COMMENTS))
       };
     }
     return itemsOfListOfPhotos;
@@ -87,7 +92,7 @@
   var commentsCount = bigPicture.querySelector('.social__comment-count');
   var commentsLoader = bigPicture.querySelector('.comments-loader');
 
-  bigPicture.classList.remove('hidden');
+  // bigPicture.classList.remove('hidden');
 
   commentsCount.classList.add('visually-hidden');
   commentsLoader.classList.add('visually-hidden');
@@ -139,4 +144,185 @@
   };
 
   generatePhotoPage(generateListOfPhotos(PHOTO_ITEMS_COUNT));
+
+  // задание 4-2
+
+  // загрузка изображения и показ формы редактирования
+  var uploadForm = document.querySelector('.img-upload__form');
+  var uploadFile = uploadForm.querySelector('#upload-file');
+  var uploadOverlayForm = uploadForm.querySelector('.img-upload__overlay');
+  var closeButtonUploadOverlayForm = uploadOverlayForm.querySelector('#upload-cancel');
+  var hashtagFieldset = uploadForm.querySelector('.img-upload__text');
+  var hashtagTextField = hashtagFieldset.querySelector('input[name=hashtags]');
+  var effectLevel = uploadForm.querySelector('.img-upload__effect-level');
+  var effectLevelPin = effectLevel.querySelector('.effect-level__pin');
+  var effectLevelDepth = effectLevel.querySelector('.effect-level__depth');
+  var effectLevelLine = effectLevel.querySelector('.effect-level__line');
+  var effectRadioButton = uploadForm.querySelector('.effects__radio');
+  var effectLevelValue = uploadForm.querySelector('.effect-level__value');
+  var imageUploadPreview = uploadForm.querySelector('.img-upload__preview');
+  var photoEffects = uploadForm.querySelector('.img-upload__effects');
+  var scaleControlSmaller = uploadForm.querySelector('.scale__control--smaller');
+  var scaleControlBigger = uploadForm.querySelector('.scale__control--bigger');
+  var scaleControlValue = uploadForm.querySelector('.scale__control--value');
+
+  var openUploadOverlayForm = function () {
+    uploadOverlayForm.classList.remove('hidden');
+    effectLevel.classList.add('visually-hidden');
+    document.addEventListener('keydown', onDocumentKeydown);
+    closeButtonUploadOverlayForm.addEventListener('click', onCloseButtonUploadOverlayFormClick);
+    hashtagTextField.addEventListener('input', validateHashtag);
+    uploadForm.addEventListener('submit', onUploadFormSubmit);
+    effectLevelPin.addEventListener('mouseup', onEffectLevelPinMouseup);
+    effectRadioButton.addEventListener('change', onEffectRadioButtonsChange);
+    photoEffects.addEventListener('change', onPhotoEffectsChange);
+    effectLevelDepth.style.width = '100%';
+    effectLevelPin.style.left = '100%';
+    scaleControlSmaller.addEventListener('click', onScaleControlSmallerClick);
+    scaleControlBigger.addEventListener('click', onScaleControlBiggerClick);
+    scaleControlValue.setAttribute('value', '100%');
+  };
+
+  var closeUploadOverlayForm = function () {
+    uploadFile.value = '';
+    uploadOverlayForm.classList.add('hidden');
+    document.removeEventListener('keydown', onDocumentKeydown);
+    closeButtonUploadOverlayForm.removeEventListener('click', onCloseButtonUploadOverlayFormClick);
+    hashtagTextField.removeEventListener('input', validateHashtag);
+    uploadForm.removeEventListener('submit', onUploadFormSubmit);
+    effectLevelPin.removeEventListener('mouseup', onEffectLevelPinMouseup);
+    effectRadioButton.removeEventListener('change', onEffectRadioButtonsChange);
+    photoEffects.removeEventListener('change', onPhotoEffectsChange);
+    scaleControlSmaller.removeEventListener('click', onScaleControlSmallerClick);
+    scaleControlBigger.removeEventListener('click', onScaleControlBiggerClick);
+  };
+
+  var onDocumentKeydown = function (evt) {
+    if (evt.code === 'Escape') {
+      if (evt.target === hashtagTextField) {
+        evt.stopPropagation();
+      } else {
+        closeUploadOverlayForm();
+      }
+    }
+  };
+
+  var onUploadFileChange = function () {
+    openUploadOverlayForm();
+  };
+
+  var onCloseButtonUploadOverlayFormClick = function () {
+    closeUploadOverlayForm();
+  };
+
+  uploadFile.addEventListener('change', onUploadFileChange);
+
+  // пропорция для определения уровня эффекта
+  var onEffectLevelPinMouseup = function () {
+    var widthOfEffectLevelLine = effectLevelLine.getBoundingClientRect().width;
+    var pinPosition = effectLevelPin.offsetLeft;
+    effectLevelValue.value = (pinPosition * 100) / widthOfEffectLevelLine + '%';
+  };
+
+  // сброс уровня эффекта до начального состояния (100%)
+  var onEffectRadioButtonsChange = function () {
+    var maxPinPosition = effectLevelLine.getBoundingClientRect().right;
+    effectLevelValue.value = (maxPinPosition * 100) / maxPinPosition + '%';
+  };
+
+  // добавление класса на картинку
+  var onPhotoEffectsChange = function () {
+    var effects = photoEffects.elements;
+
+    imageUploadPreview.classList.remove('effects__preview--none', 'effects__preview--chrome', 'effects__preview--sepia', 'effects__preview--marvin', 'effects__preview--phobos', 'effects__preview--heat');
+
+    for (var i = 0; i < effects.length; i++) {
+      if (effects[i].checked) {
+        switch (effects[i].value) {
+          case ('none'):
+            imageUploadPreview.classList.add('effects__preview--none');
+            effectLevel.classList.add('visually-hidden');
+            break;
+          case ('chrome'):
+            imageUploadPreview.classList.add('effects__preview--chrome');
+            effectLevel.classList.remove('visually-hidden');
+            break;
+          case ('sepia'):
+            imageUploadPreview.classList.add('effects__preview--sepia');
+            effectLevel.classList.remove('visually-hidden');
+            break;
+          case ('marvin'):
+            imageUploadPreview.classList.add('effects__preview--marvin');
+            effectLevel.classList.remove('visually-hidden');
+            break;
+          case ('phobos'):
+            imageUploadPreview.classList.add('effects__preview--phobos');
+            effectLevel.classList.remove('visually-hidden');
+            break;
+          case ('heat'):
+            imageUploadPreview.classList.add('effects__preview--heat');
+            effectLevel.classList.remove('visually-hidden');
+            break;
+        }
+      }
+    }
+  };
+
+  // валидация хеш-тегов
+  var onUploadFormSubmit = function (evt) {
+    evt.preventDefault();
+    validateHashtag();
+    uploadForm.submit();
+  };
+
+  var validateHashtag = function () {
+    var hashtagTextFieldContent = hashtagTextField.value;
+    var lowerCaseHashtagsList = hashtagTextFieldContent.toLowerCase();
+    var hashtagsList = lowerCaseHashtagsList.split(' ');
+
+    if (hashtagsList.length > MAX_HASHTAGS_COUNT) {
+      hashtagTextField.setCustomValidity('максимум 5 хэш-тегов');
+    } else {
+      for (var i = 0; i < hashtagsList.length; i++) {
+        if (hashtagsList[i][0] !== '#' || hashtagsList[0][0] !== '#') {
+          hashtagTextField.setCustomValidity('хэш-тег начинается с символа #');
+        } else if (hashtagsList[i] === '#') {
+          hashtagTextField.setCustomValidity('хеш-тег не может состоять только из одной решётки');
+        } else if (hashtagsList.indexOf(hashtagsList[i]) !== i) {
+          hashtagTextField.setCustomValidity('один и тот же хеш-тег не может быть использован дважды');
+        } else if (hashtagsList[i].length > MAX_HASHTAG_LENGTH) {
+          hashtagTextField.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');
+        } else {
+          hashtagTextField.setCustomValidity('');
+        }
+      }
+    }
+  };
+
+  // масштабирование картинки
+  var onScaleControlSmallerClick = function () {
+    var currentImageSize = parseInt(scaleControlValue. value, 10);
+    var setImageSize = currentImageSize - SCALE_STEP;
+    if (setImageSize <= SCALE_STEP) {
+      setImageSize = MIN_SCALE;
+    }
+    currentImageSize = setImageSize;
+    changeImageSize(currentImageSize);
+  };
+
+  var onScaleControlBiggerClick = function () {
+    var currentImageSize = parseInt(scaleControlValue. value, 10);
+    var setImageSize = currentImageSize + SCALE_STEP;
+    if (setImageSize >= MAX_SCALE) {
+      setImageSize = MAX_SCALE;
+    }
+    currentImageSize = setImageSize;
+    changeImageSize(setImageSize);
+  };
+
+  var changeImageSize = function (imageSize) {
+    imageUploadPreview.classList.remove('transform:scale(0.25)', 'transform:scale(0.5)', 'transform:scale(0.75)', 'transform:scale(1)');
+    imageUploadPreview.classList.add('transform' + ':' + 'scale' + '(' + imageSize / 100 + ')');
+    scaleControlValue.value = imageSize + '%';
+  };
 })();
