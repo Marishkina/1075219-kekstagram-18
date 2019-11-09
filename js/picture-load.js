e97c0867e52c445beb87abddea0c9ed785f58ca0
Marina <marishkina@gmail.com>
2019-11-09 19:08:09 +0800

    Задание 6-2: нужно подкачаться, также правит открытие картинок по ентеру

---
+++ js/picture-load.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+
+  window.picturesList = document.querySelector('.pictures');
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+  // вставка данных в миниатюру
+  var generatePictureElement = function (itemsListPhoto, i) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').id = 'm' + i;
+    pictureElement.querySelector('.picture__img').src = itemsListPhoto.url;
+    pictureElement.querySelector('.picture__likes').textContent = itemsListPhoto.likes;
+    pictureElement.querySelector('.picture__comments').textContent = String(itemsListPhoto.comments.length);
+
+    return pictureElement;
+  };
+
+  // отрисовка миниатюры
+  window.renderPicture = function (generateListItems) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < generateListItems.length; i++) {
+      fragment.appendChild(generatePictureElement(generateListItems[i], i));
+    }
+    window.picturesList.appendChild(fragment);
+  };
+})();
