c6924c632299d8d907e7ded92caad46e8ce57f88
Marina <marishkina@gmail.com>
2019-11-13 21:37:36 +0800

    Правки после rebase

--- js/picture-upload-form.js
+++ js/picture-upload-form.js
@@ -66,6 +66,7 @@
 
   var onDocumentKeydown = function (evt) {
 <<<<<<< HEAD:js/picture-upload-form.js
+<<<<<<< HEAD:js/picture-upload-form.js
     if (evt.code === 'Escape') {
       if (document.activeElement === hashtagTextField || document.activeElement === commentsField) {
         evt.stopPropagation();
@@ -82,6 +83,14 @@
     } else {
       window.util.isEscEvent(evt, closeUploadOverlayForm);
 >>>>>>> Задание 5-2: модуляция:js/form.js
+=======
+    if (evt.code === 'Escape') {
+      if (document.activeElement === hashtagTextField || document.activeElement === commentsField) {
+        evt.stopPropagation();
+      } else {
+        closeUploadOverlayForm();
+      }
+>>>>>>> Правки после rebase:js/form.js
     }
   };
 
@@ -245,7 +254,7 @@
     scaleControlValue.value = imageSize + '%';
   };
 
-  var validateComment = function () {
+  var onCommentsFieldChange = function () {
     if (commentsField.value.length > MAX_COMMENT_LENGTH) {
       commentsField.setCustomValidity('максимальная длина комментария 140 символов');
     } else {
@@ -254,15 +263,17 @@
   };
 
   // валидация хеш-тегов
-  var validateHashtag = function () {
+  var onHashtagTextFieldInput = function () {
     var hashtagTextFieldContent = hashtagTextField.value;
     var hashtagsList = hashtagTextFieldContent.toLowerCase().split(' ');
+
+    hashtagTextField.setCustomValidity('');
 
     if (hashtagsList.length > MAX_HASHTAGS_COUNT) {
       hashtagTextField.setCustomValidity('максимум 5 хэш-тегов');
     } else {
       for (var i = 0; i < hashtagsList.length; i++) {
-        if ((hashtagsList[i][0] !== '#' || hashtagsList[0][0] !== '#')) {
+        if (hashtagsList[i][0] !== '#') {
           hashtagTextField.setCustomValidity('хэш-тег начинается с символа #');
         } else if (hashtagsList[i] === '#') {
           hashtagTextField.setCustomValidity('хеш-тег не может состоять только из одной решётки');
@@ -270,8 +281,6 @@
           hashtagTextField.setCustomValidity('один и тот же хеш-тег не может быть использован дважды');
         } else if (hashtagsList[i].length > MAX_HASHTAG_LENGTH) {
           hashtagTextField.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');
-        } else {
-          hashtagTextField.setCustomValidity('');
         }
       }
     }
